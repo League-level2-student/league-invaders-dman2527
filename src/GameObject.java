8d189798274d100bf58176cd60ac871c9eb6f1d9
root <root@f90f2f612717>
2021-04-02 03:01:10 +0000

    in prog

---
+++ src/GameObject.java
@@ -0,0 +1,13 @@
+
+public class GameObject {
+	 int x;
+	 int y;
+	 int width;
+	 int height;
+	 int speed = 0;
+	 boolean isActive = true;
+	 public GameObject(int x, int y, int width, int height) {
+			
+	 }
+
+}
